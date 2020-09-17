d39fab39ec4602664a2b0893d5bb8ddcb192db77
yj <yj.willy.lin@gmail.com>
2020-09-17 15:09:49 +0800

    move from AODP(another repo)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ckandj.wsgi --log-file -
---
+++ static/js/ministry.js
@@ -0,0 +1,26 @@
+// 應用開放來源的層級與部會分析
+var divElement = document.getElementById('viz1596640125506');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width = '100%';
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+// 應用開放來源的中央層級之部會分析
+var divElement = document.getElementById('viz1596640241842');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width = '100%';
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+// 應用開放來源的地方層級之部會分析 
+var divElement = document.getElementById('viz1596640446205');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width = '100%';
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
